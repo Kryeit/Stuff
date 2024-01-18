4944260ec153909fcec7b045de52bc8bd927d32b
muriplz <46825658+muriplz@users.noreply.github.com>
2024-01-18 07:03:30 +0100

    Fixed /tps for real

--- src/main/java/com/kryeit/stuff/command/TPS.java
+++ src/main/java/com/kryeit/stuff/command/TPS.java
@@ -1,23 +1,21 @@
 package com.kryeit.stuff.command;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.tree.CommandNode;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 
 public class TPS {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        // Get the command node for /spark
-        CommandNode<ServerCommandSource> sparkNode = dispatcher.getRoot().getChild("spark");
-
-        if (sparkNode != null) {
-            // Get the subcommand node for /tps
-            CommandNode<ServerCommandSource> tpsNode = sparkNode.getChild("tps");
-
-            if (tpsNode != null) {
-                // Register the /tps command as an alias to /spark tps
-                dispatcher.register(CommandManager.literal("tps").redirect(tpsNode));
-            }
-        }
+        dispatcher.register(CommandManager.literal("tps")
+                .executes(context -> {
+                    // Execute the /spark tps command programmatically
+                    String command = "spark tps";
+                    context.getSource().getServer().getCommandManager().executeWithPrefix(
+                            context.getSource().withSilent(), command
+                    );
+                    return 1; // Indicate success
+                })
+        );
     }
 }
+
