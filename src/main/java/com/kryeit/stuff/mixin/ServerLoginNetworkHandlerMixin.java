51ffd3f2742b401fa4ca730d67accf016def0ea1
muriplz <46825658+muriplz@users.noreply.github.com>
2024-03-25 17:26:37 +0100

    Fixed discord link on welcome message

--- src/main/java/com/kryeit/stuff/mixin/ServerLoginNetworkHandlerMixin.java
+++ src/main/java/com/kryeit/stuff/mixin/ServerLoginNetworkHandlerMixin.java
@@ -59,7 +59,7 @@ public class ServerLoginNetworkHandlerMixin {
         player.sendMessage(Text.literal(" - Mission system (use /missions)").formatted(Formatting.AQUA));
         player.sendMessage(Text.literal(" - Teleport system (use /post and /setpost)").formatted(Formatting.AQUA));
         player.sendMessage(Text.literal("For more information: https://kryeit.com/discord, in #guides forum channel")
-                .setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://kryeit.com/discord")))
+                .setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://discord.gg/d3qXRGB8Uy")))
         );
         player.sendMessage(Text.literal("To contribute to Kryeit's development see /donate").formatted(Formatting.AQUA));
         player.sendMessage(Text.literal("Read the /rules and have fun!").formatted(Formatting.AQUA));
