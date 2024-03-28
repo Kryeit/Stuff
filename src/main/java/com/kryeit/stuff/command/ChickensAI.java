bf9a58f917337def84e93d19fd28ae3e4bf9edc4
muriplz <46825658+muriplz@users.noreply.github.com>
2024-03-28 05:59:30 +0100

    Update ChickensAI.java

--- src/main/java/com/kryeit/stuff/command/ChickensAI.java
+++ src/main/java/com/kryeit/stuff/command/ChickensAI.java
@@ -1,6 +1,7 @@
 package com.kryeit.stuff.command;
 
 import com.griefdefender.api.GriefDefender;
+import com.griefdefender.api.User;
 import com.griefdefender.api.claim.Claim;
 import com.kryeit.stuff.afk.AfkPlayer;
 import com.mojang.brigadier.Command;
@@ -35,7 +36,8 @@ public class ChickensAI {
         Claim claim = GriefDefender.getCore().getClaimAt(GriefDefender.getCore().getWorldUniqueId(world)
                 , (int) player.getX(), (int) player.getY(), (int) player.getZ());
 
-        if (claim == null || !claim.canBreak(player, player.getBlockPos(), null) || claim.isWilderness()) {
+        User user = GriefDefender.getCore().getUser(player.getUuid());
+        if (claim == null || !claim.canBreak(player, player.getBlockPos(), user) || claim.isWilderness()) {
             Supplier<Text> message = () -> Text.of("You can't use this command here");
             source.sendFeedback(message, false);
             return 0;
