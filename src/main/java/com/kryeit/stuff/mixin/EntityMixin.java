06e23aceee939b722c81d02844f83b8425954eb8
muriplz <46825658+muriplz@users.noreply.github.com>
2024-05-08 16:17:56 +0200

    Ported No-NaN-Mod to Fabric 1.20.1

---
+++ src/main/java/com/kryeit/stuff/mixin/EntityMixin.java
@@ -0,0 +1,24 @@
+package com.kryeit.stuff.mixin;
+
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+    @ModifyArg(method = "readNbt", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;setPos(DDD)V"), index = 0)
+    public double modifyPosX(double value) {
+        return Double.isFinite(value) ? value : 0;
+    }
+
+    @ModifyArg(method = "readNbt", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;setPos(DDD)V"), index = 1)
+    public double modifyPosY(double value) {
+        return Double.isFinite(value) ? value : 0;
+    }
+
+    @ModifyArg(method = "readNbt", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;setPos(DDD)V"), index = 2)
+    public double modifyPosZ(double value) {
+        return Double.isFinite(value) ? value : 0;
+    }
+}
