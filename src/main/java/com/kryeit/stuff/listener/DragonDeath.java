d6df1a5aece6e685609fb3db22c72c9c09fd14bd
muriplz <46825658+muriplz@users.noreply.github.com>
2024-05-31 08:03:06 +0200

    Fix dragon not dropping eltras when killed with a projectile

--- src/main/java/com/kryeit/stuff/listener/DragonDeath.java
+++ src/main/java/com/kryeit/stuff/listener/DragonDeath.java
@@ -1,9 +1,11 @@
 package com.kryeit.stuff.listener;
 
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.stat.Stats;
@@ -13,9 +15,19 @@ public class DragonDeath implements ServerLivingEntityEvents.AfterDeath {
     @Override
     public void afterDeath(LivingEntity entity, DamageSource damageSource) {
         if (entity.getType().equals(EntityType.ENDER_DRAGON)) {
-            if (damageSource.getSource() == null) return;
-            if (damageSource.getSource().getType().equals(EntityType.PLAYER)) {
-                ServerPlayerEntity player = (ServerPlayerEntity) damageSource.getSource();
+            Entity source = damageSource.getSource();
+            if (source == null) return;
+
+            ServerPlayerEntity player = null;
+            if (source.getType().equals(EntityType.PLAYER)) {
+                player = (ServerPlayerEntity) source;
+            } else if (source instanceof ProjectileEntity projectile) {
+                if (projectile.getOwner() instanceof ServerPlayerEntity) {
+                    player = (ServerPlayerEntity) projectile.getOwner();
+                }
+            }
+
+            if (player != null) {
                 int dragonsKilled = player.getStatHandler().getStat(Stats.KILLED.getOrCreateStat(EntityType.ENDER_DRAGON));
                 if (dragonsKilled == 1) {
                     player.giveItemStack(Items.ELYTRA.getDefaultStack());
