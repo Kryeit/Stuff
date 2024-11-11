2ad93b162c885243e0dce881af53c0749817158d
pipeman <80647470+MrRedRhino@users.noreply.github.com>
2024-11-11 15:27:10 +0100

    Fix things

--- src/main/java/com/kryeit/stuff/compat/BluemapImpl.java
+++ src/main/java/com/kryeit/stuff/compat/BluemapImpl.java
@@ -1,13 +1,24 @@
 package com.kryeit.stuff.compat;
 
 import de.bluecolored.bluemap.api.BlueMapAPI;
+import de.bluecolored.bluemap.api.markers.MarkerSet;
 
 import java.util.UUID;
 
 public class BluemapImpl {
+    public static final MarkerSet trainsMarkerSet = MarkerSet.builder()
+            .label("Trains")
+            .build();
+
     public static void changePlayerVisibility(UUID id, boolean visible) {
         BlueMapAPI.getInstance().ifPresent(api -> {
             api.getWebApp().setPlayerVisibility(id, visible);
         });
+    }
+
+    static {
+        BlueMapAPI.getInstance()
+                .flatMap(api -> api.getMap("world"))
+                .ifPresent(map -> map.getMarkerSets().put("trains", trainsMarkerSet));
     }
 }
--- src/main/java/com/kryeit/stuff/mixin/PortingLibBaseMixin.java
+++ src/main/java/com/kryeit/stuff/mixin/PortingLibBaseMixin.java
@@ -6,14 +6,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PortingLibBase.class)
+@Mixin(value = PortingLibBase.class, remap = false)
 public class PortingLibBaseMixin {
-    @Inject(method = "onInitialize", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/api/command/v2/ArgumentTypeRegistry;registerArgumentType(Lnet/minecraft/util/Identifier;Ljava/lang/Class;Lnet/minecraft/command/argument/serialize/ArgumentSerializer;)V"), cancellable = true, remap = false)
+    @Inject(method = "onInitialize", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/api/command/v2/ArgumentTypeRegistry;registerArgumentType(Lnet/minecraft/util/Identifier;Ljava/lang/Class;Lnet/minecraft/command/argument/serialize/ArgumentSerializer;)V", remap = true), cancellable = true)
     public void unregisterPortingLibArgumentTypes(CallbackInfo ci) {
         ci.cancel();
     }
 
-    @Inject(method = "onInitialize", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/api/event/Event;register(Ljava/lang/Object;)V"), cancellable = true, remap = false)
+    @Inject(method = "onInitialize", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/api/event/Event;register(Ljava/lang/Object;)V"), cancellable = true)
     public void unregisterPortingLibConfigCommand(CallbackInfo ci) {
         ci.cancel();
     }
