a26ec25714644d77f37ea95de207a8bae5f5d830
pipeman <80647470+MrRedRhino@users.noreply.github.com>
2024-11-11 13:52:52 +0100

    Fix velocity compatibility

---
+++ src/main/java/com/kryeit/stuff/mixin/PortingLibBaseMixin.java
@@ -0,0 +1,20 @@
+package com.kryeit.stuff.mixin;
+
+import io.github.fabricators_of_create.porting_lib.PortingLibBase;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PortingLibBase.class)
+public class PortingLibBaseMixin {
+    @Inject(method = "onInitialize", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/api/command/v2/ArgumentTypeRegistry;registerArgumentType(Lnet/minecraft/util/Identifier;Ljava/lang/Class;Lnet/minecraft/command/argument/serialize/ArgumentSerializer;)V"), cancellable = true, remap = false)
+    public void unregisterPortingLibArgumentTypes(CallbackInfo ci) {
+        ci.cancel();
+    }
+
+    @Inject(method = "onInitialize", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/api/event/Event;register(Ljava/lang/Object;)V"), cancellable = true, remap = false)
+    public void unregisterPortingLibConfigCommand(CallbackInfo ci) {
+        ci.cancel();
+    }
+}
