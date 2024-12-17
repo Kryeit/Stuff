15ec9db7e82da474d2e751e8ddb2c8680f2f2a94
muriplz <46825658+muriplz@users.noreply.github.com>
2024-12-17 13:23:02 +0100

    DENY BY TAG

--- src/main/java/com/kryeit/stuff/Stuff.java
+++ src/main/java/com/kryeit/stuff/Stuff.java
@@ -89,6 +89,6 @@ public class Stuff implements DedicatedServerModInitializer {
         AllConfigs.server().kinetics.maxBlocksMoved.set(6144);
         AllConfigs.server().trains.maxTrackPlacementLength.set(128);
         AllConfigs.server().schematics.maxSchematicPacketSize.set(1024);
-        AllConfigs.server().fluids.bottomlessFluidMode.set(FluidManipulationBehaviour.BottomlessFluidMode.ALLOW_BY_TAG);
+        AllConfigs.server().fluids.bottomlessFluidMode.set(FluidManipulationBehaviour.BottomlessFluidMode.DENY_BY_TAG);
     }
 }
